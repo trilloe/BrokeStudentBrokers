f35e360de8d0df1f68448e98642b4473bc19be11
Shaurya1409 <shauryasrivastava14@gmail.com>
2020-11-20 01:58:46 +0530

    Added Gauge Chart (Without Legend)

--- broke_student_brokers/lib/pages/dashboard.dart
+++ broke_student_brokers/lib/pages/dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'dart:math';
 
 class Dashboard extends StatefulWidget {
   @override
@@ -9,13 +10,62 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        // appBar: CustomAppBar(height: 80),
-        // body: Container(
-        //   width: 1000,
-        //   height: 1000,
-        //   child: SvgPicture.asset('assets/images/BSB_Logo.svg'),
-        // ),
-        );
+    return Container(height: 250, child: GaugeChart.withSampleData());
   }
+}
+
+class GaugeChart extends StatelessWidget {
+  final List<charts.Series> seriesList;
+  final bool animate;
+
+  GaugeChart(this.seriesList, {this.animate});
+
+  /// Creates a [PieChart] with sample data and no transition.
+  factory GaugeChart.withSampleData() {
+    return new GaugeChart(
+      _createSampleData(),
+      // Disable animations for image tests.
+      animate: false,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new charts.PieChart(seriesList,
+        animate: animate,
+        // Configure the width of the pie slices to 30px. The remaining space in
+        // the chart will be left as a hole in the center. Adjust the start
+        // angle and the arc length of the pie so it resembles a gauge.
+        defaultRenderer: new charts.ArcRendererConfig(
+            arcRatio: 0.12, startAngle: 3 / 2 * pi, arcLength: 2 * pi));
+  }
+
+  /// Create one series with sample hard coded data.
+  static List<charts.Series<GaugeSegment, String>> _createSampleData() {
+    final data = [
+      new GaugeSegment('GOOGL', 90, charts.Color(r: 89, g: 189, b: 204)),
+      new GaugeSegment('FB', 90, charts.Color(r: 74, g: 143, b: 213)),
+      new GaugeSegment('AAPL', 90, charts.Color(r: 255, g: 106, b: 129)),
+      new GaugeSegment('TSLA', 90, charts.Color(r: 235, g: 75, b: 138)),
+    ];
+
+    return [
+      new charts.Series<GaugeSegment, String>(
+        id: 'Segments',
+        domainFn: (GaugeSegment segment, _) => segment.segment,
+        measureFn: (GaugeSegment segment, _) => segment.size,
+        colorFn: (GaugeSegment segment, _) => segment.color,
+        data: data,
+      )
+    ];
+  }
+}
+
+/// Sample data type.
+class GaugeSegment {
+  final String segment;
+  final int size;
+  final charts.Color color;
+
+  GaugeSegment(this.segment, this.size, this.color);
 }
