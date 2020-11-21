7625f3f6da34df82ef40cef67b4f79217ecd5fe5
Pragya Datt <pragz01@gmail.com>
2020-11-22 00:30:35 +0530

    Firebase connection done

--- broke_student_brokers/lib/pages/profile.dart
+++ broke_student_brokers/lib/pages/profile.dart
@@ -1,4 +1,10 @@
+import 'package:broke_student_brokers/pages/dashboard.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'home.dart';
+import 'somethingWrong.dart';
+import 'loading.dart';
 
 class Profile extends StatefulWidget {
   @override
@@ -6,12 +12,25 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        // appBar: AppBar(
-        //   title: Text('Profile'),
-        // ),
-        );
+    return FutureBuilder(
+        // Initialize FlutterFire
+        future: _initialization,
+        builder: (context, snapshot) {
+          // Check for errors
+          if (snapshot.hasError) {
+            return somethingWrong();
+          }
+
+          // Application
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Dashboard();
+          }
+
+          //Otherwise while waiting
+          return loading();
+        });
   }
 }
---
+++ broke_student_brokers/lib/pages/somethingWrong.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class somethingWrong extends StatefulWidget {
+  @override
+  _somethingWrongState createState() => _somethingWrongState();
+}
+
+class _somethingWrongState extends State<somethingWrong> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text('Something Went Wrong'));
+  }
+}
