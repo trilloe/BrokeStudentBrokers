5321d00188f78982202a7cdc557d61ec9d41803f
Shaurya1409 <shauryasrivastava14@gmail.com>
2020-12-11 05:38:49 +0530

    Fixed Service Path in Current_Value

--- backend/build.py
+++ backend/build.py
@@ -20,7 +20,7 @@ def set_properties(project):
 
 @init
 def set_properties(project):
-    project.version = "2.5"
+    project.version = "2.6"
     project.set_property("coverage_break_build", False)
     project.depends_on("numpy")
     project.depends_on("datetime")
